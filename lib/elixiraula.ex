d650cb8137b9548c88305b1116719322267f8d0a
s4hlo <rafael.nunes@tarken.ag>
2025-09-25 11:12:51 -0300

    Refactor Elixiraula module to Elx and add matrix manipulation functions including to_matrix, get_rows_columns, and sum_matrix.

--- lib/elixiraula.ex
+++ lib/elixiraula.ex
@@ -1,4 +1,4 @@
-defmodule Elixiraula do
+defmodule Elx do
   @moduledoc """
   Documentation for `Elixiraula`.
   """
@@ -20,7 +20,60 @@ defmodule Elixiraula do
     case Enum.count(ns) do
       ^n ->
         {:ok, for(k <- 1..n, x <- ns, into: %{}, do: {k, x})}
+
+      _ ->
         :error
     end
+  end
+
+  # exemple -> to_matrix(2, 3, [1, 2, 3, 4, 5, 6]) -> {:ok, %{{0,0} => 1, {0,1} => 2, {0,2} => 3, {1,0} => 4, {1,1} => 5, {1,2} => 6}}
+  def to_matrix(n, m, ls) do
+    if Enum.count(ls) == n * m do
+      result =
+        for i <- 0..(n - 1), j <- 0..(m - 1), into: %{} do
+          index = i * m + j
+          element = Enum.at(ls, index)
+          {{i, j}, element}
+        end
+
+      {:ok, result}
+    else
+      :error
+    end
+  end
+
+  # exemple -> get_rows_columns(%{{0,0} => 1, {0,1} => 2, {0,2} => 3, {1,0} => 3, {1,1} => 4, {1,2} => 5}) -> {2, 3}
+  def get_rows_columns(matrix) do
+    {indices, _values} = Enum.unzip(matrix)
+    max_row = indices |> Enum.map(fn {row, _col} -> row end) |> Enum.max()
+    max_col = indices |> Enum.map(fn {_row, col} -> col end) |> Enum.max()
+    {max_row + 1, max_col + 1}
+  end
+
+  # exemple -> sum_matrix(%{{0,0} => 1, {0,1} => 2, {1,0} => 3, {1,1} => 4}, %{{0,0} => 1, {0,1} => 2, {1,0} => 3, {1,1} => 4}) -> {:ok, %{{0,0} => 2, {0,1} => 4, {1,0} => 6, {1,1} => 8}}
+  def sum_matrix(matrix1, matrix2) do
+    if Enum.count(matrix1) == Enum.count(matrix2) do
+      {n, m} = get_rows_columns(matrix1)
+      {n2, m2} = get_rows_columns(matrix2)
+      if n == n2 and m == m2 do
+        result =
+          for i <- 0..(n - 1), j <- 0..(m - 1), into: %{} do
+            {{i, j}, matrix1[{i, j}] + matrix2[{i, j}]}
+          end
+        {:ok, result}
+      else
+        :error
+      end
+    else
+      :error
+    end
+  end
+
+  def caller() do
+    {:ok, matrix1} = to_matrix(3, 3, [1, 2, 3, 4, 5, 6,7,8,9])
+    # sum_matrix(matrix1, matrix2)
+    # IO.inspect(matrix1)
+    # get_rows_columns(matrix1)
+    sum_matrix(matrix1, matrix1)
   end
 end
